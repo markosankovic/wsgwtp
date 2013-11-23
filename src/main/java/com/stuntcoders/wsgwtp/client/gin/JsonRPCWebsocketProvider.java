f505ae912174bb0a2ae4ff160b7392d50b7617dd
Marko Sanković <sankovic.marko@gmail.com>
2013-11-23 08:59:20 +0100

    Enable WebSocket connection logging on client side

--- src/main/java/com/stuntcoders/wsgwtp/client/gin/JsonRPCWebsocketProvider.java
+++ src/main/java/com/stuntcoders/wsgwtp/client/gin/JsonRPCWebsocketProvider.java
@@ -1,9 +1,11 @@
 package com.stuntcoders.wsgwtp.client.gin;
 
+import java.util.Date;
+import java.util.logging.Logger;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONParser;
-import com.google.gwt.user.client.Window;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.web.bindery.event.shared.EventBus;
@@ -13,6 +15,9 @@ import com.stuntcoders.wsgwtp.client.event.JsonRPCResponseEvent;
 
 public class JsonRPCWebsocketProvider implements Provider<Websocket> {
 
+    private static Logger logger = Logger
+            .getLogger(JsonRPCWebsocketProvider.class.getName());
+
     @Inject
     EventBus eventBus;
 
@@ -21,15 +26,17 @@ public class JsonRPCWebsocketProvider implements Provider<Websocket> {
         /**
          * Example: ws://localhost:8080/wsgwtp/
          */
-        String websocketBaseURL = GWT.getHostPageBaseURL().replace("http://", "ws://");
+        String websocketBaseURL = GWT.getHostPageBaseURL().replace("http://",
+                "ws://");
 
-        Websocket socket = new Websocket(websocketBaseURL + "/JsonRPCWebsocketEndpoint");
+        Websocket socket = new Websocket(websocketBaseURL
+                + "/JsonRPCWebsocketEndpoint");
 
         socket.addListener(new WebsocketListener() {
 
             @Override
             public void onClose() {
-                Window.alert("onClose");
+                logger.info("WebSocket connection closed on: " + new Date());
             }
 
             @Override
@@ -40,7 +47,7 @@ public class JsonRPCWebsocketProvider implements Provider<Websocket> {
 
             @Override
             public void onOpen() {
-                // Window.alert("onOpen");
+                logger.info("WebSocket connection opened on: " + new Date());
             }
         });
 
